74474ac740bf18188d787bb7c6e22faf3e7fd94e
NehaM <nehamalokar19@gmail.com>
2025-11-18 22:41:49 +0530

    added List datatype in the python file

--- neha.py
+++ neha.py
@@ -4,3 +4,12 @@ sample_string="I love my India"
 print(f"sample_string is {sample_string}")
 
 
+
+# Add List 
+
+Li = ["Pune" , "Mumbai", 12,50,"Delhi"]
+print("Li is-",Li)
+
+print(f"first element of the list is- {Li[0]}")
+print(f"Find the lenght of List-{len(Li)}")
+
