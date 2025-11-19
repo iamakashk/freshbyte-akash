0d37d0692196c123c20a81d951a15db78d14fdd7
NehaM <nehamalokar19@gmail.com>
2025-11-19 20:54:21 +0530

    added python script to print information of a student

---
+++ information-file.py
@@ -0,0 +1,7 @@
+print("print information of a student")
+
+name=input("enter your name ")
+age=input("enter your age ")
+address=input("enter your address ")
+
+print(f"Your name is {name} and age is {age} and you live in {address}.")
