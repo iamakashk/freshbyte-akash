8697daf01f5a879e293c7060d4e458b29ef590b0
shubham <sshubhamwadhawe@gmail.com>
2025-11-20 17:57:14 +0530

    User-Information

---
+++ user-information.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Enter your name:"
+read name
+
+echo "Enter your place:"
+read place
+
+echo "Enter your age:"
+read age
+
+echo ""
+echo "===== USER INFORMATION ====="
+echo "Name: $name"
+echo "Place: $place"
+echo "Age: $age"
+
